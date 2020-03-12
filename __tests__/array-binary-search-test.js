431d783a4feedd99159b23a1139fcd7b75e7b388
Nathan Martel <nathanmartel@gmail.com>
2020-03-12 01:56:52 -0700

    a range of tests passing

--- __tests__/array-binary-search-test.js
+++ __tests__/array-binary-search-test.js
@@ -1,9 +1,25 @@
 const binarySearch = require('../challenges/array-binary-search/array-binary-search.js');
 
-const arrayOne = [1, 2, 3, 4, 5];
-
 describe('binary search', () => {
-  it('returns an index of 1', () => {
-    expect(binarySearch(2, arrayOne)).toEqual(1);
+  it('finds the key in the middle of an array', () => {
+    expect(binarySearch(3, [1, 2, 3, 4, 5])).toEqual(2);
+  }); 
+  it('finds the key in the low middle of an even-numbered length array', () => {
+    expect(binarySearch(3, [1, 2, 3, 4, 5, 6])).toEqual(2);
+  }); 
+  it('finds the key in the high middle of an even-numbered length array', () => {
+    expect(binarySearch(4, [1, 2, 3, 4, 5, 6])).toEqual(3);
+  }); 
+  it('finds the key in the bottom half of an array', () => {
+    expect(binarySearch(2, [1, 2, 3, 4, 5, 6])).toEqual(1);
+  }); 
+  it('finds the key in the top half of an array', () => {
+    expect(binarySearch(5, [1, 2, 3, 4, 5, 6])).toEqual(4);
+  }); 
+  it('finds the key at the end of an array', () => {
+    expect(binarySearch(6, [1, 2, 3, 4, 5, 6])).toEqual(5);
+  }); 
+  it('works for strings, too', () => {
+    expect(binarySearch('gamma', ['alpha', 'beta', 'gamma'])).toEqual(2);
   }); 
 });
